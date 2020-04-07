3164201f6bc3ca0321b2f66149bfd03154196166
Иван <60978801+evuen1@users.noreply.github.com>
2020-04-07 12:22:37 +0700

    i

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    // ========================================================================
+    // плавная прокрутка по якорным ссылкам
+
+    let linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
+        V = 0.6; // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
+    for (var i = 0; i < linkNav.length; i++) {
+        linkNav[i].addEventListener('click', function (e) { //по клику на ссылку
+            e.preventDefault(); //отменяем стандартное поведение
+            var w = window.pageYOffset, // производим прокрутка прокрутка
+                hash = this.href.replace(/[^#]*(.*)/, '$1'); // к id элемента, к которому нужно перейти
+            t = document.querySelector(hash).getBoundingClientRect().top, // отступ от окна браузера до id
+                start = null;
+            requestAnimationFrame(step); // подробнее про функцию анимации [developer.mozilla.org]
+            function step(time) {
+                if (start === null) start = time;
+                var progress = time - start,
+                    r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+                window.scrollTo(0, r);
+                if (r != w + t) {
+                    requestAnimationFrame(step)
+                } else {
+                    location.hash = hash // URL с хэшем
+                }
+            }
+        }, false);
+    }
+
+    // ========================================================================
+    // импорт html to html
+
+    $('#header').load("../views/components/header.html");
+    $('#aside').load("../views/components/sidebar.html");
+    $('#footer').load("../views/components/footer.html");
+
+    let $page_title = $('title').text(); // получение содержимого title страницы
+    $('title').text(`Title ` + $page_title); // вывод в title $название_сайта + содержиое title 
+
+    $('.accordion').click(function () {
+        $(this).find('.accordion-content').toggleClass('show')
+    })
+
+    $('input[type=tel]').mask('+7 (999) 999-99-99');
+
+    $('input[type=card_n]').mask('9999 9999 9999 9999');
+    $('input[type=card_d]').mask('99/99');
+    $('input[type=card_c]').mask('999');
+
+});
